6a90907da988d9d4adf5acc157c58c4a08ba7380
meen6789z <124431515+meen6789z@users.noreply.github.com>
2025-03-18 15:35:00 +0700

    Build(File)
    Add File

---
+++ Cluster.py
@@ -0,0 +1,9 @@
+# Import Library
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+from sklearn import preprocessing
+from sklearn.cluster  import KMeans
+from sklearn.decomposition import PCA
